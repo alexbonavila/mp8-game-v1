78beecc64411834793e6d5f1453cfaf404302d11
alex <alexbonavila44@gmail.com>
2017-05-10 20:02:11 +0200

    Caregant el primer tilemap que conte ground + background

--- src/states/Game.js
+++ src/states/Game.js
@@ -4,31 +4,23 @@ import Mushroom from '../sprites/Mushroom'
 
 export default class extends Phaser.State {
   init () {}
-  preload () {}
+  preload () {
+    this.load.tilemap('tilemap', 'assets/tiled_map1.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.image('tiles', 'assets/P6yozhP.png');
+  }
 
   create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
 
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    this.map = this.game.add.tilemap('tilemap');
+    this.map.addTilesetImage('P6yozhP', 'tiles');
 
-    this.game.add.existing(this.mushroom)
+    this.backgroundlayer = this.map.createLayer('BackgroundLayer');
+    this.groundLayer = this.map.createLayer('GroundLayer');
   }
 
   render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
-    }
+    // if (__DEV__) {
+    //   this.game.debug.spriteInfo(this.mushroom, 32, 32)
+    // }
   }
 }
